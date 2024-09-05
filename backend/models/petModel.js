99d4ec456cda6b58d8a050e4ca3a359da71278a3
Fernanda <fecoimbra96@gmail.com>
2024-09-05 20:55:14 -0300

    feat: pet model ok

--- backend/models/petModel.js
+++ backend/models/petModel.js
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
 
+// Definição do schema para a tabela de pets
+const petSchema = new mongoose.Schema({
+    especie: { type: String, required: true },
+    nome: { type: String, required: true },
+    sexo: { type: String, required: true },
+    cidade: { type: String, required: true },
+    estado: { type: String, required: true },
+    descricao: { type: String },
+    imagem: { type: String, required: true },
+    status: { type: Boolean, default: true } // true = disponível para adoção
+});
+
+const Pet = mongoose.models.Pet || mongoose.model('Pet', petSchema);
+
+export default Pet;
