9d7b4f4c186b948c25cf4d898a30ab1d7b6906a3
Fernanda <fecoimbra96@gmail.com>
2024-09-13 16:03:21 -0300

    fix: bug estilo petCardDisponivel

--- admin/src/components/PetCardDisponivel/PetCardDisponivel.jsx
+++ admin/src/components/PetCardDisponivel/PetCardDisponivel.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { assets } from '../../../../frontend/src/assets/assets'
+import './PetCardDisponivel.css'
 
 const PetCardDisponivel = () => {
     return (
-        <div className="new-pet">
-            <div className="new-pet-img-container" >
-                <img src={assets.nick} alt="" />
+        <div className="available-pet">
+            <div className="available-pet-img-container" >
+                <img src={assets.nick} alt="" className="available-pet-img"/>
             </div>
-            <div className="new-pet-data">
+            <div className="available-pet-data">
                 <h5>Nick</h5>
-                <div className="new-pet-specie">
-                    <span className='new-pet-label'>Espécie: <span>Gato</span></span>
+                <div className="available-pet-specie">
+                    <span className='available-pet-label'>Espécie: <span>Gato</span></span>
 
                 </div>
-                <div className="new-pet-tutor">
-                    <span className='new-pet-label'>Responsável: <span>Fernanda Souza Coimbra Machado</span></span>
+                <div className="available-pet-tutor">
+                    <span className='available-pet-label'>Responsável: <span>Fernanda Souza Coimbra Machado</span></span>
 
                 </div>
 
                 <div className="available-pet-status">
-                    <span className='new-pet-label'>Interessados: <span>5</span></span>
+                    <span className='available-pet-label'>Interessados: <span>5</span></span>
                 </div>
             </div>
 
