b28d0a54bd6ca7bf043ada3f60b27ec9672c6214
Fernanda <fecoimbra96@gmail.com>
2024-09-14 16:21:53 -0300

    style: navbar dropdown usuario

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,40 +1,55 @@
-import React from 'react'
-import { useState} from 'react';
+import React, { useContext } from 'react'
+import { useState } from 'react';
 import './Navbar.css'
-import { Cat, User } from "phosphor-react";
+import { Cat, User, SignOut, UserSquare } from "phosphor-react";
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
 
 const Navbar = () => {
 
-    const [hamburgerOn,setHamburgerOn] = useState(false);
-    return (
+    const [hamburgerOn, setHamburgerOn] = useState(false);
+    const { token } = useContext(AppContext);
 
+    return (
 
         <nav className="navbar navbar-expand-lg navbar-custom">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><Cat size={50} alt='Home'/> MIAdota</Link>
-                <button className="navbar-toggler" onClick={()=>setHamburgerOn(!hamburgerOn)} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <Link className="navbar-brand" to="/"><Cat size={50} alt='Home' /> MIAdota</Link>
+                <button className="navbar-toggler" onClick={() => setHamburgerOn(!hamburgerOn)} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className={(hamburgerOn? "show":"") + "collapse navbar-collapse nav-items"} id="navbarNavDropdown">
+                <div className={(hamburgerOn ? "show" : "") + "collapse navbar-collapse nav-items"} id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link className="nav-link" aria-current="page" href="#" to="/" onClick={()=>setHamburgerOn(false)}>Home</Link>
+                            <Link className="nav-link" aria-current="page" href="#" to="/" onClick={() => setHamburgerOn(false)}>Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/#catalogo-de-pets" onClick={()=>setHamburgerOn(false)}>Adotar</Link>
+                            <Link className="nav-link" to="/#catalogo-de-pets" onClick={() => setHamburgerOn(false)}>Adotar</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#"  onClick={()=>setHamburgerOn(false)}>Cadastrar Pet</a>
+                            <a className="nav-link" href="#" onClick={() => setHamburgerOn(false)}>Cadastrar Pet</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#footer"  onClick={()=>setHamburgerOn(false)}>Contato</a>
+                            <a className="nav-link" href="#footer" onClick={() => setHamburgerOn(false)}>Contato</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login"  onClick={()=>setHamburgerOn(false)}><User size={32} alt='Entrar'/></Link>
+                            {!token ? <button className='nav-login-button'>Entrar</button>
+                                : <div className="nav-user-profile">
+                                    <User size={32} />
+                                    <ul className="nav-profile-dropdown">
+                                        <li><span>Minha Conta</span></li>
+                                        <hr />
+                                        <li><span>Sair</span></li>
+                                    </ul>
+                                </div>
+                            }
+                            {/* <Link className="nav-link" to="/login"  onClick={()=>setHamburgerOn(false)}><User size={32} alt='Entrar'/></Link> */}
+                            {/* <Link className="nav-link" to="/login" onClick={() => setHamburgerOn(false)}>
+
+                            </Link> */}
                         </li>
-                       
+
                     </ul>
                 </div>
             </div>
