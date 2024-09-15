1d632ff405d56322bad86546be386aefdbfa54e2
Fernanda <fecoimbra96@gmail.com>
2024-09-15 01:09:03 -0300

    feat: fetch lista pets com interesse na pág do usuario

--- frontend/src/components/MinhasAdocoesCard/MinhasAdocoesCard.jsx
+++ frontend/src/components/MinhasAdocoesCard/MinhasAdocoesCard.jsx
@@ -2,31 +2,35 @@ import React from 'react'
 import './MinhasAdocoesCard.css'
 import { assets } from '../../assets/assets'
 
-const MinhasAdocoesCard = () => {
+const MinhasAdocoesCard = ({pet,url}) => {
 
 
     return (
         <div className='user-wanted-pet'>
             <div className="user-pet-img-container">
-                <img src={assets.nick} alt="" />
+                <img src={url + "/images/" + pet.imagem} alt="" />
             </div>
             <div className="user-pet-data">
-                <h5>Nick</h5>
+                <h5>{pet.nome}</h5>
                 <div className="user-pet-specie">
-                    <span className='user-pet-label'>Espécie: <span>Gato</span></span>
+                    <span className='user-pet-label'>Espécie: <span>{pet.especie}</span></span>
 
                 </div>
                 <div className="user-pet-tutor">
-                    <span className='user-pet-label'>Responsável: <span>Fernanda Souza Coimbra Machado</span></span>
+                    <span className='user-pet-label'>Responsável: <span>{pet.responsavel}</span></span>
 
                 </div>
                 <div className="user-pet-local">
-                    <span className='user-pet-label'>Local: <span>Cataguases-MG</span></span>
+                    <span className='user-pet-label'>Local: <span>{pet.cidade}-{pet.estado}</span></span>
 
                 </div>
                 <div className="user-pet-adoption-status">
                     <span className='user-pet-label'>Status da adoção:</span>
-                    <span className='adoption-status'>Em análise</span>
+                    {
+                        pet.adotado===false?<span className='adoption-status'>Em análise</span>
+                        :<span className='adoption-status'>Adotado</span>
+                    }
+                    
                 </div>
             </div>
 
--- frontend/src/pages/AdocoesUsuario/AdocoesUsuario.jsx
+++ frontend/src/pages/AdocoesUsuario/AdocoesUsuario.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './AdocoesUsuario.css'
 import MinhasAdocoesCard from '../../components/MinhasAdocoesCard/MinhasAdocoesCard'
-import { Sidebar } from 'phosphor-react'
 import UserSidebar from '../../components/UserSidebar/UserSidebar'
+import { AppContext } from '../../context/AppContext'
+import { useContext } from 'react'
+import axios from 'axios'
 
 const AdocoesUsuario = () => {
+
+    const { token, url } = useContext(AppContext);
+    const [petsDeInteresse, setPetsDeInteresse] = useState([]);
+
+
+
+    useEffect(() => {
+        const fetchPetsDeInteresse = async () => {
+            try {
+                const response = await axios.get(`${url}/user/list`, {
+                    headers: {
+                        Authorization: `Bearer ${token}` // Token de autenticação
+                    }
+                })
+
+                if (response.data.success) {
+
+                    setPetsDeInteresse(response.data.data);
+
+                }
+            } catch (error) {
+                console.error("Erro ao listar pets de interesse", error);
+            }
+        }
+        fetchPetsDeInteresse();
+    }, [token,url])
+
     return (
         <div className='user-adoptions-page'>
             <UserSidebar />
             <div className="user-adoptions-page-content">
-                <MinhasAdocoesCard />
+                {petsDeInteresse.length > 0 ? (
+                    petsDeInteresse.map(pet => (
+                        <MinhasAdocoesCard key={pet._id} pet={pet} url={url}/>
+                    ))
+                ) : (
+                    <p>Você ainda não tem nenhum pet de interesse.</p>
+                )}
+
             </div>
         </div>
     )
