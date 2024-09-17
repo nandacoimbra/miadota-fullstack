59e2714441086e40516c607edfaa5b4e9259160a
Fernanda <fecoimbra96@gmail.com>
2024-09-17 18:35:23 -0300

    style: responsividade detalhes pet

--- frontend/src/components/DetalhesPet/DetalhesPet.jsx
+++ frontend/src/components/DetalhesPet/DetalhesPet.jsx
@@ -6,31 +6,35 @@ import './DetalhesPet.css'
 
 const DetalhesPet = ({ id, imagem, nome, sexo, cidade, estado, descricao, especie, url, manifestarInteresse }) => {
     return (
-        <div className='pet-details-container'>
-            <div className="pet-details-header-content">
-                <div className="pet-details-header">
-                    <h3>Detalhes do Animal</h3>
-                    <h5>{nome}</h5>
+        <div className="pet-details">
+            <div className='pet-details-container'>
+                <div className="pet-details-header-content">
+                    <div className="pet-details-header">
+                        <h3>Detalhes do Animal</h3>
+                        <h5>{nome}</h5>
+                    </div>
+                    <div className="pet-details-content">
+                        <p className='pet-details-description'>
+                            {descricao}
+                        </p>
+                        <ul className='pet-details-info'>
+                            <li> <img src={assets.dna} alt="" /> {especie}</li>
+                            <li> <img src={assets.gender} alt="" /> {sexo}</li>
+                            <li> <MapPinLine size={32} /> {cidade}-{estado}</li>
+                            <li>Responsável: Fernanda</li>
+                        </ul>
+                    </div>
+                    <button className='pet-details-adopt-button' onClick={() => manifestarInteresse(id)}>Tenho interesse</button>
                 </div>
-                <div className="pet-details-content">
-                    <p className='pet-details-description'>
-                        {descricao}
-                    </p>
-                    <ul className='pet-details-info'>
-                        <li> <img src={assets.dna} alt="" /> {especie}</li>
-                        <li> <img src={assets.gender} alt="" /> {sexo}</li>
-                        <li> <MapPinLine size={32} /> {cidade}-{estado}</li>
-                        <li>Responsável: Fernanda</li>
-                    </ul>
-                </div>
-                <button className='pet-details-adopt-button' onClick={()=>manifestarInteresse(id)}>Tenho interesse</button>
+
             </div>
             <div className="pet-details-img-container">
                 <img src={url + "/images/" + imagem} alt="" className="pet-details-img" />
             </div>
-
         </div>
+
     )
 }
+
 
 export default DetalhesPet
