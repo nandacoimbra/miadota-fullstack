d3fd0579e585db3989cea0253be9446cfe6bcd03
Fernanda <fecoimbra96@gmail.com>
2024-09-17 01:19:08 -0300

    styles: responsividade home page login e cadastro de usuario

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Footer.css'
 import { Cat, Copyright, InstagramLogo, FacebookLogo, Envelope, User } from "phosphor-react";
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Footer = () => {
+
+    const navigate = useNavigate();
     return (
         <footer className="footer" id="footer">
             <div className="footer-container">
@@ -12,11 +17,10 @@ const Footer = () => {
                 </div>
                 <div className="footer-container-middle">
                     <ul className='navlinks'>
-                        <li>Home</li>
-                        <li>Adotar</li>
-                        <li>Cadastrar Pet</li>
-                        <li>Sobre</li>
-                        <li><User size={25} /></li>
+                        <li onClick={()=>{navigate("/")}}>Home</li>
+                        <li onClick={()=>{navigate("/#catalogo-de-pets")}}>Adotar</li>
+                        <li onClick={()=>{navigate("/user/newpet")}}>Cadastrar Pet</li>
+                        
                     </ul>
                 </div>
                 <div className="footer-container-right">
